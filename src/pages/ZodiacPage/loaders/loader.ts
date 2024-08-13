e46d3b8e743f57211e58401b5a0bc57171743c74
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2024-08-13 22:20:29 +0300

    fix loader

--- src/pages/ZodiacPage/loaders/loader.ts
+++ src/pages/ZodiacPage/loaders/loader.ts
@@ -1,12 +1,11 @@
 import { LoaderFunction, LoaderFunctionArgs } from "react-router-dom"
 import { zodiacApi } from "../api/zodiac"
 import { Language } from "../../../shared/types/language"
-import { useTranslation } from "react-i18next"
+import i18n from "i18next"
 
 export const loader: LoaderFunction<any> = async ({
   params,
 }: LoaderFunctionArgs<{ params: { zodiacId: string } }>) => {
-  const { i18n } = useTranslation()
   const lang = i18n.language as Language | undefined
   const res = await zodiacApi.getZodiacDescription(params.zodiacId, lang)
   return res.json()
